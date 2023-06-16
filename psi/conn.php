2978a20a56099de4ebcde39f162ea36d40bb9c8e
Kyizaky <126072531+Kyizaky@users.noreply.github.com>
2023-06-16 14:38:18 +0700

    Delete psi directory

--- psi/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","psi");
-if(!$con){
-	die("Failed to Establish Database Connection");
-
-}
-	//konek db
--- psi/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-session_destroy();
-header("Location:tampilan_login.php");
-//now login part completed
