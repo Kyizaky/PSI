704cbcc33cacf7a2f7de3393e32eb2b4b5c3fc1e
Kyizaky <126072531+Kyizaky@users.noreply.github.com>
2023-06-16 14:39:00 +0700

    Add files via upload

---
+++ admin/pegawai.php
@@ -0,0 +1,3 @@
+<a href="reservasi.php" class="btn btn-primary" role="button">Tambah Data</a> <br>
+<a href="logout.php" class="btn btn-primary" role="button">logout</a>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
