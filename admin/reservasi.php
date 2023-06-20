533a737cc2528ef507fc696a901d55b67f702670
Octaviano <215232220@students.uii.ac.id>
2023-06-20 19:44:56 +0700

    pelanggan

--- admin/reservasi.php
+++ admin/reservasi.php
@@ -145,7 +145,7 @@
 
         //Kondisi apakah berhasil atau tidak dalam mengeksekusi query diatas
         if ($hasil) {
-            header("Location:pegawai.php");
+            header("Location:pelanggan.php");
         }
         else {
             echo "<div class='alert alert-danger'> Data Gagal disimpan.</div>";
